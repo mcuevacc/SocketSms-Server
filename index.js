7d6f700146f64ca8fd23b4fdb845f00fb6f75d4f
Miguel <miguel.cueva.l@uni.pe>
2018-12-15 10:32:32 -0500

    code finish

---
+++ index.js
@@ -0,0 +1,36 @@
+var net = require('net');
+var sockets = [];
+var HOST = '127.0.0.1';
+var PORT = 6969;
+
+var server = net.createServer();
+server.listen(PORT, HOST);
+console.log('Server listening on ' + HOST +':'+ PORT);
+
+server.on('connection', function(socket) {
+
+	socket.name = socket.remoteAddress + ":" + socket.remotePort 
+    sockets.push(socket);
+
+    console.log('CONNECTED: ' + socket.remoteAddress +':'+ socket.remotePort);
+    
+    socket.on('data', function(data) {
+        console.log('DATA ' + socket.remoteAddress + ': ' + data);
+
+        obj = JSON.parse(data);
+	    if(typeof(obj.number)!=='undefined' && typeof(obj.text)!=='undefined'){
+	        sockets.forEach(function(socket, index, array){
+	            socket.write(data);
+	        });
+	    }
+    });
+
+    socket.on('end', function() {
+        console.log(socket.name + " left the broadcast.\n");
+        sockets.splice(sockets.indexOf(socket), 1);
+    });
+
+    socket.on('error', function(error) {
+        console.log('Socket got problems: ', error.message);
+    });
+});
