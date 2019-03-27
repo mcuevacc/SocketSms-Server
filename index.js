3d5e617a98d6e9f13bf0d32875f3fdb83b5753e3
Miguel <miguel.cueva.l@uni.pe>
2019-03-26 22:34:44 -0500

    fix

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var net = require('net');
 var sockets = [];
+var senders = [];
 var HOST = '0.0.0.0';
 var PORT = 8080;
 
@@ -9,17 +10,28 @@ console.log('Server listening on '+HOST+':'+PORT);
 
 server.on('connection', function(socket) {
 
-	socket.name = socket.remoteAddress+":"+socket.remotePort;
+    console.log('CONNECTED: '+socket.remoteAddress +':'+ socket.remotePort+"\n");
+
+    socket.name = socket.remoteAddress+":"+socket.remotePort; 
     sockets.push(socket);
 
-    console.log('CONNECTED: '+socket.remoteAddress +':'+ socket.remotePort+"\n");
-    
     socket.on('data', function(data) {
         try {
             console.log('DATA '+socket.remoteAddress + ': ' + data);
+            
+            if(data=='Sender'){
+                senders.push(socket);
+                return;
+            }else{
+                if(senders.length!=0)
+                    socket.write('Send');                
+                else
+                    socket.write('Error');
+            }
+                        
             obj = JSON.parse(data);
     	    if(typeof(obj.number)!=='undefined' && typeof(obj.text)!=='undefined'){
-    	        sockets.forEach(function(socket, index, array){
+    	        senders.forEach(function(socket, index, array){
     	            socket.write(data);
     	        });
     	    }
@@ -29,8 +41,10 @@ server.on('connection', function(socket) {
     });
 
     socket.on('end', function() {
-        console.log('Client '+socket.name+" left the broadcast.\n");
+        console.log(socket.name+" left the broadcast.\n");
         sockets.splice(sockets.indexOf(socket), 1);
+        if(senders.indexOf(socket)!=-1)
+            senders.splice(senders.indexOf(socket), 1);
     });
 
     socket.on('error', function(error) {
